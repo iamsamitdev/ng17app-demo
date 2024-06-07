d88a434b7e94dc36375e02599e2e05f7b05b0655
Samit Koyom <samitkoyom@gmail.com>
2024-06-08 00:11:32 +0700

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core'
+import { 
+  ReactiveFormsModule, 
+  FormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  // FormGroup
+  loginForm!: FormGroup
+  // สร้างตัวแปรไว้เช็คว่า submit form หรือยัง
+  submitted = false
+
+  // ตัวแปรสำหรับผูกกับฟอร์ม
+  userLogin = {
+    "email": "",
+    "password": ""
+  }
+
+  // Constructor
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+  }
+
+  // ngOnInit
+  ngOnInit(){
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  submitLogin(){
+
+    this.submitted = true
+
+    // ถ้าฟอร์มไม่ถูกต้อง (Invalid)
+    if(this.loginForm.invalid){
+      return
+    } else {
+      this.userLogin.email = this.loginForm.value.email
+      this.userLogin.password = this.loginForm.value.password
+
+      if(this.userLogin.email == "admin@email.com" && this.userLogin.password == "123456"){
+        alert("Login Success")
+      } else {
+        alert("Login Fail")
+      }
+
+    }
+  }
+
+  resetForm(){
+    this.submitted = false
+    this.loginForm.reset()
+  }
+
+}
